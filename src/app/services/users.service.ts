91b1604ca49151634626dd7904fe6d6bd32cfbbc
sebastien baudet <seb.baudet76@gmail.com>
2023-04-22 11:04:39 +0200

    fin modifcation login et create user form

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,16 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { environments } from 'src/environnements/env';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsersService {
+  env = environments;
   constructor(private http: HttpClient) {}
 
   adduser(form: FormData) {
-    //const url = this.env.url + 'api/register/';
-    const url = 'http://127.0.0.1:8000/api/register/';
+    const url = this.env.url + 'api/register/';
     return this.http.post(url, form);
   }
 }
